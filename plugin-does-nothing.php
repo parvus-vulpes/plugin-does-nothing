a27f0c240cfcb32ddbc3f08158c020ee78256c09
Mikko Mörö <48695378+sivuseppa@users.noreply.github.com>
2024-08-15 15:12:46 +0300

    Create plugin-does-nothing.php

---
+++ plugin-does-nothing.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name: Plugin Does Nothing
+ * Description: This is an empty plugin fo testing purposes and does nothing.
+ * Version: 1.0
+ * Author: Nobody
+ * License: GPL2
+ *
+ * @package Plugin_Does_Nothing
+ */
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
